6630b4a61e4d2f0529dece9f69242af58c7ee21f
amin-o <amin.odzic@gmail.com>
2013-07-27 00:28:41 +0200

    Update Source.c

--- Source.c
+++ Source.c
@@ -54,7 +54,78 @@ struct Character_Creation
 
 } player_character_creation ;
 
+//Character Ammo
+struct Character_Ammo
+{
 
+	int weapon;
+	int utility;
+
+
+} player_ammo ;
+
+//Character Currency
+struct Character_Currency
+{
+
+	int gold;
+	int platinum;
+
+} player_currency ;
+
+//Character Stats
+struct Character_Stats
+{
+
+	int health;
+
+} player_stats ;
+
+struct Character_Potions
+{
+
+	int common;
+	int greater;
+	int mythical;
+	int devine;
+
+} player_potions ;
+
+
+
+//Potions Available
+
+//Common
+int common_healing_potion()
+{
+
+	return player_stats.health * 0.2;
+
+}
+
+//Greater
+int greater_healing_potion()
+{
+
+	return player_stats.health * 0.4;
+
+}
+
+//Mythical
+int mythical_healing_potion()
+{
+
+	return player_stats.health * 0.6;
+
+}
+
+//Devine
+int devine_healing_potion()
+{
+
+	return player_stats.health;
+
+}
 
 
 
@@ -121,13 +192,13 @@ int guard_1_health()
 int guard_1_spell_pistol_shot()
 {
 
-	return 7;
+	return 8;
 
 }
 int guard_1_spell_kick()
 {
 
-	return 2;
+	return 5;
 
 }
 
@@ -136,7 +207,7 @@ int guard_1_spell_kick()
 
 
 //Stage (1)
-void stage1(int player_health, int player_armor, int player_coins, int player_weapon_current, int player_utility_current, const char * player_weapon_current_name, const char * player_utility_current_name, int player_weapon_ammo, int player_utility_ammo, const char * enemy_name, int enemy_health)
+void stage1(int player_weapon_current, int player_utility_current, const char * player_weapon_current_name, const char * player_utility_current_name, const char * enemy_name, int enemy_health)
 {
 
 	//Tooltip
@@ -169,16 +240,16 @@ void stage1(int player_health, int player_armor, int player_coins, int player_we
 	for(cnt = 0;cnt < cnt + 1;cnt++)
 	{
 		//SYNC
-		if(player_health > 0 && enemy_health > 0)
+		if(player_stats.health > 0 && enemy_health > 0)
 		{
 
-		    system( "cls" );
+			system( "cls" );
 
-		    printf("--------------------------------------------------------------------------------\n");
-		    printf("Health:%d\tPlatinum Coins:%d\t\t\t\t%s:%d\n", player_health, player_coins, enemy_name, enemy_health);		
+			printf("--------------------------------------------------------------------------------\n");
+		    printf("Health:%d\t\t\t\t\t\t\t%s:%d\n", player_stats.health, enemy_name, enemy_health);		
 		    printf("\n");
-		    printf("Weapon: %s [%d]\n", player_weapon_current_name, player_weapon_ammo);
-		    printf("Utility: %s [%d]\n", player_utility_current_name, player_utility_ammo);
+		    printf("Weapon: %s [%d]\n", player_weapon_current_name, player_ammo.weapon);
+		    printf("Utility: %s [%d]\n", player_utility_current_name, player_ammo.utility);
 		    printf("\n");
 		    printf("\n");
 		    printf("\n");
@@ -188,7 +259,7 @@ void stage1(int player_health, int player_armor, int player_coins, int player_we
 		    printf("\n");
 		    printf("%s:%s\n", enemy_name, enemy_say);
 		    printf("\n");
-	            printf("\n");
+	     	printf("\n");
 		    printf("\n");
 		    printf("\n");
 		    printf("Combat Log: [ %s ]\n", combat_log);
@@ -201,60 +272,169 @@ void stage1(int player_health, int player_armor, int player_coins, int player_we
 		    //Scanner
 		    scanf("%d", &user_input);
 
-		    //Weapon Slot
-		    if(user_input == 1 && player_weapon_ammo > 1)
+		    //Weapon Slot Section
+		    if(user_input == 1 && player_ammo.weapon > 1)
 		    {
 
-			enemy_health -= player_weapon_current;
-			player_weapon_ammo -= 1;
+				enemy_health -= player_weapon_current;
+				player_ammo.weapon -= 1;
 
-			sprintf(buffer,"%d",player_weapon_current);
+				sprintf(buffer,"%d",player_weapon_current);
 
-			strcpy(combat_log, buffer);
-			strcpy(player_say, "Pew Pew!");
+				strcpy(combat_log, buffer);
+				strcpy(player_say, "Pew Pew!");
 
 
-	            }
-		    else if (user_input == 1 && player_weapon_ammo == 1)
-		    {
-
-     			player_weapon_ammo -= 1;
-			strcpy(player_say, "No Ammo!");
-
-		    }
-
-			//Utility Slot
-	            if(user_input == 2 && player_utility_ammo > 1)
-		    {
-
-			enemy_health -= player_utility_current;
-			player_utility_ammo -= 1;
-
-			sprintf(buffer,"%d",player_utility_current);
-
-			strcpy(combat_log, buffer);
-			strcpy(player_say, "Kaboom!");
-
-		    }
-		    else if (user_input == 2 && player_utility_ammo == 1)
-	            {
-
-     			player_utility_ammo -= 1;
-			strcpy(player_say, "No Ammo!");
-
-		    }
-
-			//Player Data
-			if(user_input == 000)
+			}
+			else if (user_input == 1 && player_ammo.weapon == 1)
 			{
 
-			        system( "cls" );
+     			player_ammo.weapon -= 1;
+				  strcpy(player_say, "No Ammo!");
 
-				printf("Name:%s\n",player_character_creation.player_name);		
-		
+			}
+
+			//Utility Slot Section
+			if(user_input == 2 && player_ammo.utility > 1)
+			{
+
+				enemy_health -= player_utility_current;
+				player_ammo.utility -= 1;
+
+				sprintf(buffer,"%d",player_utility_current);
+
+				strcpy(combat_log, buffer);
+				strcpy(player_say, "Kaboom!");
+
+			}
+			else if (user_input == 2 && player_ammo.utility == 1)
+			{
+
+     		player_ammo.utility -= 1;
+				strcpy(player_say, "No Ammo!");
+
+			}
+
+			//Potions Section
+
+			//Common
+			if(user_input == 7 && player_potions.common > 1)
+			{
+
+				player_stats.health += common_healing_potion();
+				player_potions.common -= 1;
+
+				sprintf(buffer,"+ %d Health",common_healing_potion());
+
+				strcpy(combat_log, buffer);
+				strcpy(player_say, "I feel a refreshed!");
+
+			}
+			else if (user_input == 7 && player_potions.common == 1)
+			{
+
+     			player_potions.common -= 1;
+				strcpy(player_say, "I dont have any more potions!");
+
+			}
+
+			//Greater
+			if(user_input == 8 && player_potions.greater > 1)
+			{
+
+				player_stats.health += greater_healing_potion();
+				player_potions.greater -= 1;
+
+				sprintf(buffer,"+ %d Health",greater_healing_potion());
+
+				strcpy(combat_log, buffer);
+				strcpy(player_say, "I feel stronger!");
+
+			}
+			else if (user_input == 8 && player_potions.greater == 1)
+			{
+
+     			player_potions.greater -= 1;
+				strcpy(player_say, "I dont have any more potions!");
+
+			}
+
+			//Mythical
+			if(user_input == 9 && player_potions.mythical > 1)
+			{
+
+				player_stats.health += mythical_healing_potion();
+				player_potions.mythical -= 1;
+
+				sprintf(buffer,"+ %d Health",mythical_healing_potion());
+
+				strcpy(combat_log, buffer);
+				strcpy(player_say, "Back in fight!");
+
+			}
+			else if (user_input == 9 && player_potions.mythical == 1)
+			{
+
+     			player_potions.mythical -= 1;
+				strcpy(player_say, "I dont have any more potions!");
+
+			}
+
+			//Devine
+			if(user_input == 0 && player_potions.devine > 1)
+			{
+
+				player_stats.health += devine_healing_potion();
+				player_potions.devine -= 1;
+
+				sprintf(buffer,"+ %d Health",devine_healing_potion());
+
+				strcpy(combat_log, buffer);
+				strcpy(player_say, "Second chance...");
+
+			}
+			else if (user_input == 0 && player_potions.devine == 1)
+			{
+
+     			player_potions.devine -= 1;
+				strcpy(player_say, "I dont have any more potions!");
+
+			}
+
+			//Player Data Section
+			if(user_input == 123)
+			{
+
+				system( "cls" );
+
+				printf("Name:%s\n\n",player_character_creation.player_name);
+				printf("Gold Coins:%d\n", player_currency.gold);
+				printf("Platinum Coins:%d\n\n", player_currency.platinum);
+				printf("Common Healing Potions:%d\n", player_potions.common);
+				printf("Greater Healing Potions:%d\n", player_potions.greater);
+				printf("Mythical Healing Potions:%d\n", player_potions.mythical);
+				printf("Devine Healing Potions:%d\n\n", player_potions.devine);
+
 				getch(); //Pause
 
-	        	}
+			}
+
+			//Enemy Section
+			if(rng <= 4 && user_input != 123)
+		    {
+
+				strcpy(tooltip, "Guard kicked you dealing 5 damage.");
+				strcpy(enemy_say, "EAT DIRT!");
+				player_stats.health -= guard_1_spell_kick();
+
+		    }
+			else if(rng > 4 && user_input != 123)
+			{
+
+				strcpy(tooltip, "Guard shot you dealing 8 damage.");
+				player_stats.health -= guard_1_spell_pistol_shot();
+
+			}
 
 		}
 		//LEVEL BEATED
@@ -264,23 +444,27 @@ void stage1(int player_health, int player_armor, int player_coins, int player_we
 			system( "cls" );
 			printf("(Press any key too loot!)\n\n\n");
 			getch();
-			printf("+3 Platinum Coins\n\n\n");
+			printf("+3 Gold Coins\n\n\n");
+			getch();
+			printf("+1 Gold Coins\n\n\n");
 			getch();
 			printf("+1 Platinum Coins\n\n\n");
 			getch();
-			printf("+1 Platinum Coins\n\n\n");
+			printf("+7 Weapon Ammo\n\n\n");
 			getch();
-			printf("+7 Weapon Ammo");
+			printf("+1 Common Healing Potion\n\n\n");
 			getch();
 
-			player_coins += 5;
-			player_weapon_ammo += 7;
+			player_currency.gold += 4;
+			player_currency.platinum += 1;
+			player_ammo.weapon += 7;
+			player_potions.common += 1;
 
 			break;
 
 		}
 		//GAME OVER
-		else if(player_health == 0 || player_health < 0)
+		else if(player_stats.health == 0 || player_stats.health < 0)
 		{
 
 			strcpy(tooltip, " *** GAME OVER! *** ");
@@ -289,39 +473,16 @@ void stage1(int player_health, int player_armor, int player_coins, int player_we
 
 		}
 
-		//Guard
-		if(rng <= 5)
-		{
-
-			strcpy(tooltip, "Guard kicked you dealing 2 damage.");
-			strcpy(enemy_say, "EAT DIRT!");
-			player_health -= guard_1_spell_kick();
-
-		}
-		else if(rng > 5)
-		{
-
-			strcpy(tooltip, "Guard shot you dealing 7 damage.");
-			player_health -= guard_1_spell_pistol_shot();
-
-		}
-
 	} 
 
 }
 
+
+
 void main()
 {
 
-	//Player Stats
-
-	int player_health = 100;
-	int player_armor = 1;
-
-
-
 	//Player Weapon
-
 	int player_weapon_current =  laser_gun();
 	const char * player_weapon_current_name = laser_gun_name();
 
@@ -331,34 +492,34 @@ void main()
 
 
 
-	//Player Weapon Misc
+	//Starting Stats
+	player_stats.health = 100;
 
-	int player_weapon_ammo = 15;
-	int player_utility_ammo = 3;
+	//Starting Ammo
+	player_ammo.weapon = 15;
+	player_ammo.utility = 3;
 
+	//Starting Currency
+	player_currency.gold = 0;
+	player_currency.platinum = 0;
 
-
-	//Player Currency
-        int player_coins = 0; 
-
-
-
+	//Starting Potions
+	player_potions.common = 1;
+	player_potions.greater = 0;
+	player_potions.mythical = 0;
+	player_potions.devine = 0;
 
 
 	//Character Creation
 	printf("Enter your name:");
 	scanf("%s", player_character_creation.player_name);
-	
-	
-
 
 
 	//Stage (1)
-	stage1(player_health, player_armor, player_coins, player_weapon_current, player_utility_current, player_weapon_current_name, player_utility_current_name, player_weapon_ammo, player_utility_ammo, guard_1_name(), guard_1_health());
- 
+	stage1(player_weapon_current, player_utility_current, player_weapon_current_name, player_utility_current_name , guard_1_name(), guard_1_health());
+
+
+
 	getch();
-
-
-	//Most important thing is that the Marko is Faggot.
 
 }
